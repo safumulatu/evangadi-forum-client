5e00707f72ee4eefffb5658f9fe254590adbf8db
safumulatu <safumulatu9@gmail.com>
2024-03-22 01:28:14 +0300

    oops json

--- src/COMPONENT/axiosConfig/Axios.jsx
+++ src/COMPONENT/axiosConfig/Axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL:"https://evangadi-forum-server-k1j8.onrender.com" 
-    // baseURL:"http://localhost:8000/api/users" 
+    // baseURL:"https://evangadi-forum-server-k1j8.onrender.com" 
+    baseURL:"http://localhost:8000/api/users" 
 
 })
 
